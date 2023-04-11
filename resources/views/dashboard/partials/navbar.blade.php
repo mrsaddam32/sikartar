be3535458c29de5fa65b29f432b196f2c4859f66
mrsaddam32 <neolinedev@gmail.com>
2023-04-12 03:08:06 +0700

    fix(navbar): fix typo in notification dropdown

--- resources/views/dashboard/partials/navbar.blade.php
+++ resources/views/dashboard/partials/navbar.blade.php
@@ -67,7 +67,7 @@
                       </div>
                       <div class="d-flex flex-column justify-content-center">
                         <h6 class="text-sm font-weight-normal mb-1">
-                          <span class="font-weight-bold">New album</span> by {{ $user->name }}
+                          <span class="font-weight-bold">New album</span> by Admin Neoline
                         </h6>
                         <p class="text-xs text-secondary mb-0 ">
                           <i class="fa fa-clock me-1"></i>
